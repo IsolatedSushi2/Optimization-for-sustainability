6f2ed220519dac0c4dc300fc0ef2b4ddb68fbfea
Max van Huffelen <maxvanhuffelen@gmail.com>
2022-05-02 22:38:46 +0200

    improved code quality of dataReader. implemented a lot of generator. Still needs a function to generate individual cars.

--- dataReader.py
+++ dataReader.py
@@ -2,23 +2,25 @@ import csv
 import numpy as np
 
 def readCSVs():
-    with open('./data/arrival_hours.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
+    path_arrival_hours = './data/arrival_hours.csv'
+    path_charging_volume = './data/charging_volume.csv'
+    path_connection_time = './data/connection_time.csv'
+    path_solar = './data/solar.csv'
+    
+    def readPath(path):
+        with open(path) as csv_file:
+            csv_reader = csv.reader(csv_file, delimiter=',')
 
-        arrival_data = [castToValue(row) for row in csv_reader]
+            return [castToValue(row) for row in csv_reader]
 
-    with open('./data/charging_volume.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        charging_volume_data = [castToValue(row) for row in csv_reader]
 
-    with open('./data/connection_time.csv') as csv_file:
-        csv_reader = csv.reader(csv_file, delimiter=',')
-        connection_time_data = [castToValue(row) for row in csv_reader]
-
-    return np.array(arrival_data), np.array(charging_volume_data), np.array(connection_time_data)
+    return readPath(path_arrival_hours), readPath(path_charging_volume), readPath(path_connection_time), readPath(path_solar)
 
 def castToValue(row):
-    return [int(row[0]), float(row[1])]
+    if len(row) == 2:
+        return [int(row[0]), float(row[1])]
+    if len(row) == 3:
+        return [int(row[0]), float(row[1]), float(row[2])]
 
 
 if __name__ == "__main__":
--- generator.py
+++ generator.py
@@ -1,11 +1,36 @@
 import numpy as np
+import dataReader as dr
+from queue import PriorityQueue
 
-def generateCarArrivals(arrival_data, carAmount):
-    weights = arrival_data[:,1] * carAmount
-    print(weights)
-    samples = np.random.poisson(weights)
-    print(samples)
+def generateCar():
+    pass
 
+def generateSolarValue(timeOfDay, solar_availability_distributions, season='summer'):
+    # returns a randomly generated value for available power from a solar panel array at the specified time of day in the specified season (summer/winter)
+    timeOfDay = timeOfDay%24
+    if season=='summer':
+        solar_power = solar_availability_distributions[timeOfDay,2] * 200
+    else:
+        solar_power = solar_availability_distributions[timeOfDay,1] * 200
+    return np.random.normal(solar_power, 0.15*solar_power)
 
-    print(np.sum(samples))
+def generateAllEvents(arrival_fractions, charging_volume_distributions, connection_time_distributions, solar_availability_distributions, average_daily_cars, timeLength = 24, season='summer'):
+    # N.B. timeLength input is in HOURS not in seconds
+
+    generatedEvents = PriorityQueue()
+
+    # Generate car arrivals
+    for t in range(timeLength):
+        allMoments = np.random.poisson(average_daily_cars*arrival_fractions[t,1], size=3600)
+        newCarsIndices = np.where(allMoments>0)[0] #use this to find the indices in the array where the value is nonzero (i.e. one or more cars arrive)
+        for time in newCarsIndices:
+            for newCar in range(allMoments[time]):
+                # Loop is needed in case two cars arrive the very same second.
+                generatedEvents.put((t*3600+time, ("carArrives", (generateCar()) ) ))
+
+    # Generate solar events
+    for t in range(timeLength):
+        generatedEvents.put((t*3600, ("solarUpdate", (generateSolarValue(t, solar_availability_distributions, season)) ) ))
+
+    return generatedEvents
 
