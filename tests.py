4cad2e728172329b2837600fe5cfc848878e6001
IsolatedSushi <s.vanherpt@students.uu.nl>
2022-05-03 20:46:07 +0200

    Image for milestone and updated tests

--- tests.py
+++ tests.py
@@ -8,19 +8,21 @@ import numpy as np
 # Show the distribution of car arrivals
 def testCarArrivals():
     # Generate the events
+    hoursAmount = 24 * 14
+
     arrival_fractions, charging_volume_distributions, connection_time_distributions, solar_availability_distributions = dr.readCSVs()
     eventsQueue = generateAllEvents(arrival_fractions, charging_volume_distributions,
-                                    connection_time_distributions, solar_availability_distributions)
+                                    connection_time_distributions, solar_availability_distributions, timeLength=hoursAmount)
 
     # Get all the arrival times
     carArrivalTimes = []
     while (not eventsQueue.empty()):
-        a = eventsQueue.get()  # throw away one event
+        a = eventsQueue.get()  # get current event
         if a.eventType == "carArrives":
             carArrivalTimes.append(a.time)
 
     # Plot the arrival times in a histogram
-    plt.hist(np.array(carArrivalTimes) / 3600, density=True, bins=24)
+    plt.hist(np.array(carArrivalTimes) / 3600, density=True, bins=hoursAmount)
     plt.show()
 
 
@@ -34,6 +36,7 @@ def testCarConnectionTimes():
     #Get the connection times
     carConnectionTimes = []
     while (not eventsQueue.empty()):
+        a = eventsQueue.get()  # get current event
         if a.eventType == "carArrives":
             carConnectionTimes.append(a.data.connectionTime)
     carConnectionTimes = np.array(carConnectionTimes)
@@ -41,7 +44,7 @@ def testCarConnectionTimes():
 
 
 def main():
-    testCarConnectionTimes()
+    testCarArrivals()
 
 
 if __name__ == "__main__":
